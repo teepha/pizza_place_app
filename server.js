6684a3a54dcac437c5262b28244e3230e251f789
Lateefah Amuda <mac@Lateefahs-MacBook-Pro.local>
2020-06-16 14:01:49 +0100

    - fix build issues

--- server.js
+++ server.js
@@ -3,17 +3,15 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "build")));
 
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-}
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port, (error) => {
   if (error) throw error;
